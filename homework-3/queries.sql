bd1451cf49e3b8f48e124417fac8b1d13b7e5172
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-07-29 15:23:28 -0700

    решение заполнено

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -2,15 +2,38 @@
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
-
+SELECT
+    customers.company_name,
+    CONCAT(employees.first_name, ' ', employees.last_name) AS fullname
+FROM
+    orders
+    JOIN employees USING(employee_id)
+    JOIN customers USING(customer_id)
+    JOIN shippers ON shippers.shipper_id = orders.ship_via
+WHERE
+    employees.city = 'London'
+    AND customers.city = 'London'
+    AND shippers.company_name = 'United Package'
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock,
+suppliers.contact_name, suppliers.phone
+FROM products
+JOIN categories USING(category_id)
+JOIN suppliers USING(supplier_id)
+WHERE
+    discontinued <> 1
+	AND units_in_stock < 25
+	AND category_name IN('Condiments', 'Dairy Products')
+ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name FROM customers
+WHERE customer_id NOT IN(SELECT customer_id FROM orders)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name FROM products
+WHERE EXISTS (SELECT quantity FROM order_details WHERE order_details.product_id=products.product_id AND order_details.quantity = 10)
